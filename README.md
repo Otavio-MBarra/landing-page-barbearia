5c8d847696a82198139af4e6656aef89dade9950
Otavio-MBarra <otaviombarra@gmail.com>
2025-02-12 05:47:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+![inicio barbearia](./img/barbearia-capa.png)
+
+# Landing page barbearia
+
+uma landing page com intuito de praticar HTML e CSS.
+
+## 📁 Acesso ao projeto
+
+[Veja o projeto final](https://stalwart-cannoli-193810.netlify.app).
+
+## 🛠️ Abrir e rodar o projeto
+
+Para abrir e rodar o projeto, basta abrir o aquivo `index.html` no navegador.
+
+### Contatos
+<div>
+<a href="https://www.instagram.com/otavio.code/" target="_blank"><img src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white" target="_blank"></a>
+
+<a href="https://www.linkedin.com/in/ot%C3%A1vio-monteiro-barra/" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"></a>
+
+</div>
---
+++ index.js
@@ -0,0 +1,39 @@
+
+const debounce = function (func, wait, immediate) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animate";
+
+function animeScrll() {
+  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+  target.forEach(function (elemento) {
+    if (windowTop > elemento.offsetTop) {
+      elemento.classList.add(animationClass);
+    } else {
+      elemento.classList.remove(animationClass);
+    }
+  });
+}
+
+animeScrll();
+if (target.length) {
+  window.addEventListener(
+    "scroll",
+    debounce(function () {
+      animeScrll();
+    }, 10)
+  );
+}
